e2b847288ac175a411497df0b60607b1ede20fa5
Shafi Haque <shafihaque7@gmail.com>
2018-05-25 12:04:20 -0700

    same

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,13 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        {{-- <meta name="csrf-token" content="{{  csrf_token()   }}"> --}}
+        <meta name="csrf-token" content="{{  csrf_token()   }}">
 
-        {{-- <script>window.Laravel = { csrfToken: '{{  csrf_token()   }}'}
-        </script> --}}
+        <script>window.Laravel = { csrfToken: '{{  csrf_token()   }}'}
+        </script> 
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
