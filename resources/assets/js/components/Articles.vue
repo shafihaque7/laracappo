80811450eafe15cc83ebf967eadc2b9dd0bbb039
Shafi Haque <shafihaque7@gmail.com>
2018-05-26 12:11:50 -0700

    same

--- resources/assets/js/components/Articles.vue
+++ resources/assets/js/components/Articles.vue
@@ -61,7 +61,7 @@
                      <div class="d-flex justify-content-between align-items-center">
                        <div class="btn-group">
                          <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                         <button @click="deleteArticle(article.id)" type="button" class="btn btn-sm btn-outline-secondary">Delete</button>
+                         <button @click="deleteArticle(article.id)" @click="closeArticle()" type="button" class="btn btn-sm btn-outline-secondary">Delete</button>
                        </div>
                        <small class="text-muted">9 mins</small>
                      </div>
