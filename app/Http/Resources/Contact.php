9074e933b21a503c7ad3e8709362da2a7d35dcc6
Shafi Haque <shafihaque7@gmail.com>
2018-05-30 13:53:16 -0700

    same

--- app/Http/Resources/Contact.php
+++ app/Http/Resources/Contact.php
@@ -14,6 +14,12 @@ class Contact extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'fullName' => $this->name,
+            'mobile' => $this->phone,
+            'created_at' => (string)$this->created_at->format('m/d/Y'),
+
+        ];
     }
 }
