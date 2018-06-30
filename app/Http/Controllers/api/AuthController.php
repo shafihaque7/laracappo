515bbdf181a5165acc253aa0a2e8bd08c5d24bb7
root <root@ip-172-31-38-242.us-east-2.compute.internal>
2018-06-30 02:13:18 +0000

    From new server

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -25,7 +25,7 @@ class AuthController extends Controller
 		    'form_params' => [
 		        'grant_type' => 'password',
 		        'client_id' => '2',
-		        'client_secret' => env('OAUTH_KEY', '0dFlXVELscFl0kdo0kj7tZ7DsbergQz9BzAgUUxG'),
+		        'client_secret' => env('OAUTH_KEY', 'mrJGwm8oTdbidK6PV1AfbVk7uIkvAXxl98PQ5Sf9'),
 		        'username' => $request->email,
 		        'password' => $request->password,
 		        'scope' => '',
@@ -50,7 +50,7 @@ class AuthController extends Controller
 				'form_params' => [
 					'grant_type' => 'password',
 					'client_id' => '2',
-					'client_secret' => env('OAUTH_KEY', '0dFlXVELscFl0kdo0kj7tZ7DsbergQz9BzAgUUxG'),
+					'client_secret' => env('OAUTH_KEY', 'mrJGwm8oTdbidK6PV1AfbVk7uIkvAXxl98PQ5Sf9'),
 					'username' => $request->email,
 					'password' => $request->password,
 					'scope' => '',
@@ -69,7 +69,7 @@ class AuthController extends Controller
 		        'grant_type' => 'refresh_token',
 		        'refresh_token' => request('refresh_token'),
 		        'client_id' => '2',
-		        'client_secret' => env('OAUTH_KEY', '0dFlXVELscFl0kdo0kj7tZ7DsbergQz9BzAgUUxG'),
+		        'client_secret' => env('OAUTH_KEY', 'mrJGwm8oTdbidK6PV1AfbVk7uIkvAXxl98PQ5Sf9'),
 		        'scope' => '',
 		    ],
 		]);
